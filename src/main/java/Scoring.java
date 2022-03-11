603b3d5b78dd3c4ab431aee5186d7f80007ab6e1
User <a0981083323@gmail.com>
2022-03-11 15:42:13 +0800

    Scoring

---
+++ src/main/java/Scoring.java
@@ -0,0 +1,13 @@
+public class Scoring {
+    public static void main(String[] args) {
+        Student jack=new Student("Jack",60,55);//物件
+        //jack.name="Jack";
+        //jack.math=55;
+        //jack.english=88;
+        Student jenny=new Student("jenny",78,90);//物件
+        jack.print();
+        jenny.print();
+        Student s=new Student();//物件
+
+    }
+}
